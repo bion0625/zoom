6c3b781b634c8ae010a70202cd4e6dcebce56200
이의정 <stxtory@gmail.com>
2023-09-16 09:29:12 +0900

    all to all chatting setting

--- src/server.js
+++ src/server.js
@@ -16,11 +16,15 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Server ✅");
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on("message", (message) => console.log(message.toString()));
-    socket.send("hello!!!");
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString()))
+    });
 });
 
 server.listen(3000, handleListen);
